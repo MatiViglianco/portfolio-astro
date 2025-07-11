affa5ade9014ea1d1655ebbe6a85562ec27b0cc6
MatiViglianco <matiasviglisnco@gmail.com>
2025-07-11 01:43:39 -0300

    Corregir configuración de base para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: `https://MatiViglianco.github.io`,
+  base: `/portfolio-astro/`,
 });
