6bfe3fdb52d10746237a2b5c3780b9caf52a9f99
Matías Agustín Viglianco <matiasviglianco@gmail.com>
2026-08-14 09:13:07 -0300

    Focus portfolio on production case studies

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Matías Viglianco | Backend & AI Integrations Engineer';
+export const SITE_DESCRIPTION = 'Production AI, Django backend engineering and business automation case studies by Matías Viglianco.';
